cd9085b8e607e166eb5298a47f4cd40fd0eabf6c
zobay <aszubayr@gmail.com>
2023-07-26 18:31:55 +0600

    Create initial version with two way binding for text

---
+++ js/index.js
@@ -0,0 +1,34 @@
+const data = {
+    value: ''
+};
+
+const el = document.getElementById('inputEl');
+
+Object.defineProperty(data, 'prop', {
+    get: function () {
+        console.log('Getter called');
+        return this.value;
+    },
+    set: function (value) {
+        console.log('Setter called');
+        this.value = value;
+        el.value = value;
+        printVal();
+    }
+});
+
+
+// attaching the event listener on keyup events
+el.addEventListener('keyup', (event) => {
+    data.prop = event.target.value;
+});
+
+function printVal() {
+    const el = document.getElementById('val');
+    el.innerText = data.prop;
+}
+
+const btn = document.getElementById('incrementVal');
+btn.addEventListener('click', () => {
+    data.prop = Number(data.prop) + 1;
+});
